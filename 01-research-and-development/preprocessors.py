00d013808164ec528249b4803f0e16087bb0cecc
Mahmoud Nasser <mnasserone@gmail.com>
2023-10-01 04:18:08 +0300

    feature engineering pipelne notebook added and preprocessors python file modiefied

--- 01-research-and-development/preprocessors.py
+++ 01-research-and-development/preprocessors.py
@@ -6,50 +6,50 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class TemporalVariableTransformer(BaseEstimator, TransformerMixin):
-  # Temporal elapsed time transformer
+	# Temporal elapsed time transformer
 
-  def __init__(self, variables, reference_variable):
+    def __init__(self, variables, reference_variable):
+        
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        self.variables = variables
+        self.reference_variable = reference_variable
 
-    if not isinstance(variables, list):
-      raise ValueError('variable should be list')
+    def fit(self, X, y=None):
+        # we need this step to fit the sklearn pipeline
+        return self
 
-    self.variables = variables
-    self.reference_variable = reference_variable
+    def transform(self, X):
 
-  def fit(self, X, y=None):
-    #v we need this step to fit the sklearn pipeline
-    return self
+    	# so that we do not over-write the original dataframe
+        X = X.copy()
+        
+        for feature in self.variables:
+            X[feature] = X[self.reference_variable] - X[feature]
 
-  def transform(self, X):
-
-    # so that we do not over-write the original dataframe
-    X = X.copy()
-
-    for feature in self.variables:
-      X[feature] = X[self.reference_variable] - X[feature]
-
-    return X
+        return X
 
 
 
 # categorical missing value imputer
 class Mapper(BaseEstimator, TransformerMixin):
 
-  def __init__(self, variables, mappings):
+    def __init__(self, variables, mappings):
 
-    if not isinstance(variables, list):
-      raise ValueError('variables should be a list')
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
 
-    self.variables = variables
-    self.mappings = mappings
+        self.variables = variables
+        self.mappings = mappings
 
-  def fit(self, X, y=None):
-    # we need the fit statement to accomodate the sklearn pipeline
-    return self
+    def fit(self, X, y=None):
+        # we need the fit statement to accomodate the sklearn pipeline
+        return self
 
-  def transform(self, X):
-    X = X.copy()
-    for feature in self.variables:
-      X[feature] = X[feature].mmap(self.mappings)
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.mappings)
 
-    return X
+        return X
